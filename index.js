96b710c023839c859ba02514f2d1aa0197e3aa94
dfieker <danielmfieker@gmail.com>
2018-03-03 12:25:16 -0600

    Make our first JS program.

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var name = "Daniel";
+var born = 1976;
+var year = 2018;
+var age = year - born;
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+
+if( age > 40 ){
+	console.log("you're old");
+} else {
+	console.log("Dang...I was hoping to be younger than you.");
+} 
+	if (age == 40 ) {
+	console.log("Soon you will be old as well.");
+	} else {
+	console.log("Blah blah");
+	}
